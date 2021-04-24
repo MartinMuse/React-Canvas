870dc90a1fbb394bc24ad04ccb4e029de7240b90
MartinMuse <alex.1gorokhovsky@icloud.com>
2021-04-24 14:42:05 +0300

    fixed bugs

--- src/Circle/circle.js
+++ src/Circle/circle.js
@@ -2,7 +2,7 @@ import "./circle.css"
 
 export function Circle(props) {
   return (
-      <div className={"circle"} onMouseDown={(e) => props.onMouseDownHandler(e, e.currentTarget, 'circle')}>
+      <div className={"circle"}  onMouseDown={(e)=>props.onMouseDownHandler(e, e.currentTarget, 'circle')} >
       </div>
   )
 }
--- src/Parallelogramm/parallelogramm.js
+++ src/Parallelogramm/parallelogramm.js
@@ -1,7 +1,8 @@
 import "./parallelogramm.css"
 export function Parallelogramm(props){
   return(
-      <div className={'parallelogramm'} onMouseDown={(e) => props.onMouseDownHandler(e, e.currentTarget, 'parallelogramm')}>
+      <div className={'parallelogramm'} onMouseDown={(e) => props.onMouseDownHandler(e, e.currentTarget, 'parallelogramm')}
+           onClick={(e)=>props.onClickHandler(e)}>
         <div className={'red-stripe'}>
         </div>
       </div>
